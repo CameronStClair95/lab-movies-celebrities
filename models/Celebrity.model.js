bdc1a85c7aaf63eaed0ee8e0aa5dd53b6812d7bd
CameronStClair95 <cameronaitcheson@gmail.com>
2023-01-07 16:17:12 +0000

    It 2 & 3 Done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,16 @@
 //  Add your code here
+const {Schema, model, SchemaType} = require('mongoose');
+
+const celebritySchema = new Schema({
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+},
+
+{
+    timestaps: true
+}
+
+);
+
+module.exports = model('celebrities', celebritySchema)
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,3 +1,26 @@
 const router = require("express").Router();
+const Celebrity = require('../models/Celebrity.model')
+
+router.get("/celebrities/create", (req, res) => {
+    res.render('celebrities/new-celebrity')
+}) 
+
+router.post("/celebrities/create", (req, res) => {
+    Celebrity.create(req.body)
+    .catch(error =>
+        res.render('celebrities/create'))
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
