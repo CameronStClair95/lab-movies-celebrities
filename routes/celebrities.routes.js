f68a7bd113cd6dbd66fafa67a84a11bf174360e6
Shengwei LI <51034296+sw-li@users.noreply.github.com>
2023-01-09 15:16:39 +0100

    corrected the redirect link

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,28 +1,20 @@
 const router = require("express").Router();
-const Celebrity = require('../models/Celebrity.model')
+const Celebrity = require("../models/Celebrity.model");
 
 router.get("/celebrities/create", (req, res) => {
-    res.render('celebrities/new-celebrity')
-}) 
+  res.render("celebrities/new-celebrity");
+});
 
 router.post("/celebrities/create", (req, res) => {
-    Celebrity.create(req.body)
-    .catch(error =>
-        res.render('celebrities/create'))
-})
+  Celebrity.create(req.body)
+    .then(() => res.redirect("/celebrities"))
+    .catch((error) => res.render("celebrities/create"));
+});
 
-router.get('/celebrities', (req, res) => {
-    Celebrity.find()
-.then(result =>
-    res.render('celebrities/celebrities', {result}))
-})
-
-
-
-
-
-
-
-
+router.get("/celebrities", (req, res) => {
+  Celebrity.find().then((result) =>
+    res.render("celebrities/celebrities", { result })
+  );
+});
 
 module.exports = router;
